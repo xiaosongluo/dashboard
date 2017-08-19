e41451f2f5f8082f461ea378b24d022d9a362063
xiaosongluo <cqulxs@gmail.com>
2017-08-20 03:12:30 +0800

    use github.com/spf13/afero

--- app/utils/config/config_test.go
+++ app/utils/config/config_test.go
@@ -2,26 +2,19 @@ package config
 
 import (
 	"encoding/json"
-	"io"
-	"os"
 	"testing"
+	"github.com/spf13/afero"
 )
 
 func Test_Load_Success(t *testing.T) {
-	filepath := &mockedFilePath{}
-	io := &mockedIO{}
-	ioutil := mockedIoutil{}
 
-	filepath.reportErr = false
-	filepath.reportAbsPath = "D:\test.json"
-	io.reportErr = false
-	io.reportErrType = nil
-	io.reportFile = nil
-	ioutil.reportErr = false
-	ioutil.reportErrType = nil
-	ioutil.reportByte = []byte(`{"id":1,"name":"cat"}`)
+	appFS := afero.NewOsFs()
 
-	Load("test.json", cfg)
+	appFS.MkdirAll("fakedir/tmp", 0755)
+	afero.WriteFile(appFS, "fakedir/tmp/test.json", []byte(`{"id":1,"name":"cat"}`), 0777)
+	name := "fakedir/tmp/test.json"
+
+	Load(name, cfg)
 	if cfg.Id != 1 || cfg.Name != "cat" {
 		t.Error("Load module information wrong!")
 	}
@@ -36,42 +29,4 @@ type configuration struct {
 
 func (c *configuration) ParseJSON(b []byte) error {
 	return json.Unmarshal(b, &c)
-}
-
-type mockedFilePath struct {
-	reportErr     bool
-	reportAbsPath string
-}
-
-func (m mockedFilePath) Abs(path string) (string, error) {
-	if m.reportErr {
-		return "", os.ErrNotExist
-	}
-	return m.reportAbsPath, nil
-}
-
-type mockedIO struct {
-	reportErr     bool
-	reportErrType error
-	reportFile    *os.File
-}
-
-func (m mockedIO) Open(name string) (*os.File, error) {
-	if m.reportErr {
-		return nil, m.reportErrType
-	}
-	return m.reportFile, nil
-}
-
-type mockedIoutil struct {
-	reportErr     bool
-	reportErrType error
-	reportByte    []byte
-}
-
-func (m mockedIoutil) ReadAll(r io.Reader) ([]byte, error) {
-	if m.reportErr {
-		return nil, m.reportErrType
-	}
-	return m.reportByte, nil
 }
